f5a51e41516a4d43afcdc29b142370ca64cd85d4
Ganna DANYLOVA <gdanylov@e1r9p13.unit.ua>
2019-04-04 20:03:57 +0300

    have_token

--- rush00/rush00/ViewController.swift
+++ rush00/rush00/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    let grant_type = "client_credentials"
+    let client_id = "3d120b3541ccf35990c63c640707e57ab4f618a0c66a01f983d2bb88b81cedd0"
+    let client_secret = "8f85fcfb8aed813d0f6526f10868d202fe94db7cc272e484146cfa6f32ab2ac4"
+//    let code = "Authorization"
+//    var api : APIController?
+    var token : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        token_access()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func token_access() {
+        let bearer = ((client_id + ":" + client_secret).data(using: String.Encoding.utf8))!.base64EncodedString(options: NSData.Base64EncodingOptions())
+        let url = URL(string: "https://api.intra.42.fr/oauth/token")
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.setValue("Basic " + bearer, forHTTPHeaderField: "Authorization")
+        request.setValue("application/x-www-form-urlencoded;charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
+        
+        
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            if data != nil {
+                do {
+                    if let get : NSDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                        print(get)
+                        self.token = get["access_token"] as? String
+                        print(self.token as Any)
+                    }
+                }
+                catch (let error) {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+        
+        
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let response = response {
+//                print(response)
+//            }
+//        }
+//        task.resume()
+//    }
     }
-
-
 }
 
